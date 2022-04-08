c92ce10ba3697e126057353dbfbac7cd89c04c0b
ShinDSR <danang.wibisono.ft20mail.umy.ac.id>
2022-04-08 23:29:23 +0700

    Add project files.

---
+++ Exercise3/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3
+{
+    public partial class ProdiTI : Form
+    {
+        public ProdiTI()
+        {
+            InitializeComponent();
+        }
+
+        private void ProdiTI_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter.Fill(this.prodiTIDataSet.Mahasiswa);
+
+            dataGridView1.Columns[0].HeaderText = "No. Mahasiswa";
+            dataGridView1.Columns[1].HeaderText = "Nama Mahasiswa";
+            dataGridView1.Columns[2].HeaderText = "Alamat Mahasiswa";
+            dataGridView1.Columns[3].HeaderText = "Jenis Kelamin";
+            dataGridView1.Columns[4].HeaderText = "No. Telepon";
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 10, FontStyle.Bold);
+
+            dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Purple;
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+
+            dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+
+            dataGridView1.Columns[2].DefaultCellStyle.ForeColor = Color.Cyan;
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+
+            dataGridView1.Columns[3].DefaultCellStyle.ForeColor = Color.Green;
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.DarkRed;
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
